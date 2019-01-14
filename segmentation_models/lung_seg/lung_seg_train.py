4ceca69e940d78724631c105074d63992dd8b971
edwardyangxin <yx_yoyo@live.cn>
2019-01-14 12:24:13 +0800

    init train with augmentation

--- segmentation_models/lung_seg/lung_seg_train.py
+++ segmentation_models/lung_seg/lung_seg_train.py
@@ -10,10 +10,12 @@ from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from skimage import transform
 
+from segmentation_models.lung_seg.image_generator_keras import ImageDataGenerator
+
 if __name__ == '__main__':
-        project_root = dirname(dirname(dirname(abspath(__file__))))
-        print(project_root)
-        sys.path.insert(0, project_root)
+    project_root = dirname(dirname(dirname(abspath(__file__))))
+    print(project_root)
+    sys.path.insert(0, project_root)
 
 from segmentation_models.unet import Unet
 
@@ -91,8 +93,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 traind = 7
 vald = 2
 input_size = 256
-# dataset_path = "C:/Users/edwardyangxin/Desktop/workprojs/segmentation_models/segmentation_models/dataset/jia_jsrt_1k.hdf5"
-dataset_path = "/home/ycli/code/segmentation_models/segmentation_models/dataset/jia_jsrt_1k.hdf5"
+dataset_path = "C:/Users/edwardyangxin/Desktop/workprojs/segmentation_models/segmentation_models/dataset/jia_jsrt_1k.hdf5"
+# dataset_path = "/home/ycli/code/segmentation_models/segmentation_models/dataset/jia_jsrt_1k.hdf5"
 # prepare data
 x, y = load_data(dataset_path, input_size)
 
@@ -104,28 +106,27 @@ x_train, y_train, x_val, y_val, x_test, y_test = split_data(x, y, 0, traind, val
 model = Unet(backbone_name='resnet34', input_shape=(input_size, input_size, 3), encoder_weights='imagenet')
 model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[mean_iou, 'binary_accuracy'])
 
-model_file_format = 'weights.{epoch:02d}-{val_acc:.3f}.hdf5'
+model_file_format = 'weights.{epoch:02d}-{val_mean_iou:.3f}-{val_binary_accuracy:.3f}.hdf5'
 checkpointer = ModelCheckpoint(model_file_format, monitor='val_mean_iou', period=1)
 
 batch_size = 8
-model.fit(x_train, y_train, epochs=100, callbacks=[checkpointer],
-          steps_per_epoch=(x_train.shape[0] + batch_size - 1) // batch_size,
-          validation_data=(x_val, y_val),
-          validation_steps=(x_val.shape[0] + batch_size - 1) // batch_size)
+# model.fit(x_train, y_train, epochs=100, callbacks=[checkpointer], batch_size=8, validation_data=(x_val, y_val))
 
-# train_gen = ImageDataGenerator(rotation_range=10,
-#                                width_shift_range=0.1,
-#                                height_shift_range=0.1,
-#                                rescale=1.,
-#                                zoom_range=0.2,
-#                                fill_mode='nearest',
-#                                cval=0)
-# test_gen = ImageDataGenerator(rescale=1.)
-# # train model
-# batch_size = 8
-# model.fit_generator(train_gen.flow(x_train, y_train, batch_size),
-#                     steps_per_epoch=(x_train.shape[0] + batch_size - 1) // batch_size,
-#                     epochs=100,
-#                     callbacks=[checkpointer],
-#                     validation_data=test_gen.flow(x_val, y_val),
-#                     validation_steps=(x_val.shape[0] + batch_size - 1) // batch_size)
+train_gen = ImageDataGenerator(rotation_range=10,
+                               width_shift_range=0.1,
+                               height_shift_range=0.1,
+                               rescale=1.,
+                               zoom_range=0.2,
+                               fill_mode='nearest',
+                               cval=0,
+                               data_format='channels_last')
+test_gen = ImageDataGenerator(rescale=1.)
+# train model
+batch_size = 8
+steps = (x_train.shape[0] - batch_size + 1) // batch_size
+model.fit_generator(train_gen.flow(x_train, y_train, batch_size),
+                    steps_per_epoch=steps,
+                    epochs=100,
+                    callbacks=[checkpointer],
+                    validation_data=test_gen.flow(x_val, y_val),
+                    validation_steps=1)
